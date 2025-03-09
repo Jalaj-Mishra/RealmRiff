da6effb6817a5107e2c7370b488321d45141e67f
JALAJ MISHRA <mjalaj77@gmail.com>
2025-03-10 02:09:07 +0530

    Basic Functionality of Posts are done 15.9

--- RealmRiff/posts/models.py
+++ RealmRiff/posts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
+from genres.models import Genre
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
+
+class Post(models.Model):
+    name = models.CharField(max_length=255, unique=False),
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True),
+    content = models.TextField(),
+    content_html = models.TextField(editable=False),
+    genre = models.ForeignKey(Genre, related_name='posts', null=True, blank=True, on_delete=models.CASCADE),
+
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.content_html = self.content
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("posts:single", kwargs={"username": self.user.username, "pk": self.pk})
+
+
+    class Meta:
+        ordering = ["-created_at"]
+        unique_together = ["user", "name"]
+
+    
